83bd3845c4d2fcd69f985a2b16d89b63f985feca
Ammar Chishti <ammarchishti80@gmail.com>
2019-10-10 22:38:28 -0400

    Ensure that Due Date and Status show up in todolist items in their proper format

--- HW2 React TodoList/src/components/list_screen/ListItemCard.js
+++ HW2 React TodoList/src/components/list_screen/ListItemCard.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 
 export class ListItemCard extends Component {
+    
+    isCompleted() {
+        if (this.props.listItem.completed === true) {
+            var isCompleted = "Completed"
+        }
+        return isCompleted
+    }
+
+    isPending() {
+        if (this.props.listItem.completed === false) {
+            var isCompleted = "Pending"
+        }
+        return isCompleted
+    }
+    
     render() {
         return (
             <div className='list_item_card'>
@@ -8,13 +23,16 @@ export class ListItemCard extends Component {
                     {this.props.listItem.description}
                 </div>
                 <div className='list_item_card_assigned_to'>
-                    Assigned To: <strong>{this.props.listItem.assignedTo}</strong>
+                    Assigned To: <strong>{this.props.listItem.assigned_to}</strong>
                 </div>
                 <div className='list_item_card_due_date'>
-                    {this.props.listItem.dueDate}
+                    {this.props.listItem.due_date}
                 </div>
                 <div className='list_item_card_completed'>
-                    {this.props.listItem.completed}
+                    {this.isCompleted()}
+                </div>
+                <div className='list_item_card_not_completed'>
+                    {this.isPending()}
                 </div>
             </div>
         )
